40aa7d233d00ff5d32663b0958a4ff9dc2eb1c28
ChoiHyoSik <huchoi24@naver.com>
2021-08-20 14:01:01 +0900

    BottomNavigation 및 fragment 추가

--- app/src/main/java/kr/co/chat/MainActivity.kt
+++ app/src/main/java/kr/co/chat/MainActivity.kt
@@ -2,10 +2,48 @@ package kr.co.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kr.co.chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBottomNav()
     }
+
+
+    private fun initBottomNav() {
+
+        val homeFragment = HomeFragment()
+        val chatFragment = ChatFragment()
+        val myPageFragment = MyPageFragment()
+
+        binding.bottomNavi.setOnItemSelectedListener {
+
+            when(it.itemId) {
+
+                R.id.nav_home -> replaceFragment(homeFragment)
+
+                R.id.nav_chat -> replaceFragment(chatFragment)
+
+                R.id.nav_mypage -> replaceFragment(myPageFragment)
+            }
+
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container , fragment)
+            .commit()
+
+    }
+
 }
---
+++ app/src/main/java/kr/co/chat/MyPageFragment.kt
@@ -0,0 +1,13 @@
+package kr.co.chat
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class MyPageFragment : Fragment(R.layout.fragment_mypage) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
